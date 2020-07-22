04da229d2764d82b95c448fcc645918ff056c1d4
Allen DeHoff <allen.dehoff21@gmail.com>
2020-07-22 13:55:06 -0600

    added prompts to script for password length, uppercase letters, lowercase letters, numbers, and special characters

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ function writePassword() {
 
 }
 
+//prompt for password length
+var pwlength = prompt("How many characters would you like your new password to be? Pick a number 8 - 128.")
+//prompt for uppercase letters
+var uppercase = confirm("Would you like uppercase letters?")
+//promt for lowercase letters
+var lowercase = confirm("Would you like lowercase letters?")
+//prompt for numbers
+var nums = confirm("Would you like numbers?")
+//prompt for special characters
+var spchar = confirm("Would you like special characters?")
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
