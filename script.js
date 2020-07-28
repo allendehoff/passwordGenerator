979df61b7ce7fbd54da5bee1c8ae175b8ea6b32d
Allen DeHoff <allen.dehoff21@gmail.com>
2020-07-28 17:27:16 -0600

    changes made during tutoring session to add helper functions, ensure that at least one criteria was selected, and to shuffle the final string right before returing it to the screen. Still needs to be cleaned up before submitting

--- script.js
+++ script.js
@@ -4,75 +4,121 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-//prompt for password length
-var pwlength = prompt("How many characters would you like your new password to be? Pick a number 8 - 128.")
-// console.log(pwlength)
+  //prompt for password length
+  // var pwlength = 
+  // console.log(pwlength)
 
-//reassign pwlength to x for easier use in function
-var x = parseInt(pwlength)
-// console.log(x)
+  //reassign pwlength to x for easier use in function
+  var pwlength = parseInt(prompt("How many characters would you like your new password to be? Pick a number 8 - 128."))
+  // console.log(x)
 
-//checks to make sure user selected a valid password length and break script if they did not
-if (x === NaN || x < 8 || x > 128){
-  alert("Your password needs to be 8 - 128 charaters. Please select OK and press the 'Generate Password' button again.")
-}
-//runs script if user did select a valid password length
-else {
-//prompt for uppercase letters
-var uppercase = confirm("Would you like uppercase letters?")
-// console.log(uppercase)
+  //checks to make sure user selected a valid password length and break script if they did not
+  if (pwlength === NaN || pwlength < 8 || pwlength > 128) {
+    alert("Your password needs to be 8 - 128 charaters. Please select OK and press the 'Generate Password' button again.")
+    return;
+  }
+  //runs script if user did select a valid password length
 
-//promt for lowercase letters
-var lowercase = confirm("Would you like lowercase letters?")
-// console.log(lowercase)
+  //prompt for uppercase letters
+  var uppercase = confirm("Would you like uppercase letters?")
+  // console.log(uppercase)
 
-//prompt for numbers
-var nums = confirm("Would you like numbers?")
-// console.log(nums)
+  //promt for lowercase letters
+  var lowercase = confirm("Would you like lowercase letters?")
+  // console.log(lowercase)
 
-//prompt for special characters
-var spchar = confirm("Would you like special characters?")
-// console.log(spchar)
+  //prompt for numbers
+  var nums = confirm("Would you like numbers?")
+  // console.log(nums)
 
-//establishes variable to store selected characters
-var characters = "";
+  //prompt for special characters
+  var spchar = confirm("Would you like special characters?")
+  // console.log(spchar)
 
-//if user selects uppercase letters, adds uppercase letters to characters variable
-if(uppercase === true){
-  characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
+  // if(!(uppercase || lowercase || nums || spchar))
+  if (!uppercase && !lowercase && !nums && !spchar) {
+    return;
+  }
 
-//if user selects lowercase letters, adds lowercase letter to characters variable
-if(lowercase === true){
-  characters += "abcdefghijklmnopqrstuvwxyz"
-}
+  //establish variable to store random characters
+  var randomCharacters = ""
+  //establishes variable to store selected characters
+  var characters = "";
 
-//if user selects numbers, adds numbers to characters variable
-if(nums === true){
-  characters += "0123456789"
-}
+  //if user selects uppercase letters, adds uppercase letters to characters variable
+  if (uppercase) {
+    var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    characters += uppers
+    randomCharacters += getChar(uppers)
+  }
 
-//if uer selects special characters, adds special characters to characters variable
-if(spchar === true){
-  characters +=  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-}
-// console.log(characters)
+  //if user selects lowercase letters, adds lowercase letter to characters variable
+  if (lowercase) {
+    characters += "abcdefghijklmnopqrstuvwxyz"
+  }
 
-//establish variable to store random characters
-var randomCharacters = ""
+  //if user selects numbers, adds numbers to characters variable
+  if (nums) {
+    characters += "0123456789"
+  }
 
-//loop that generates random characters from characters variable the number of times selected by the user
-for (var i = 0; i < x; i++){
-  randomCharacters += characters.charAt(Math.floor(Math.random() * characters.length))
-}
-// console.log(randomCharacters)
+  //if uer selects special characters, adds special characters to characters variable
+  if (spchar) {
+    characters += "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
+  // console.log(characters)
 
-// creates variable targeting text area for password
+
+  //loop that generates random characters from characters variable the number of times selected by the user
+  for (var i = randomCharacters.length; i < pwlength; i++) {
+    randomCharacters += getChar(characters)
+  }
+  // console.log(randomCharacters)
+
+  // creates variable targeting text area for password
   var passwordText = document.querySelector("#password");
 
   //return result to the html text area for password
   passwordText.value = randomCharacters;
-}
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function getChar(str) {
+  return str[getRandIndex(str)]
+  // return str.charAt(Math.floor(Math.random() * str.length))
+}
+
+function getRandIndex(str) {
+  return Math.floor(Math.random() * str.length)
+}
+
+function shuffle(str) {
+  str = str.split("")
+
+  // 01234
+  // i:0 === 0 ri:3 === 3
+  // 31204
+  // i:1 === 1 ri:4 === 4
+  // 34201
+  // i:2 === 2 ri:2 === 2
+  // 34201
+  // i:3 === 0 ri:1 === 4
+  // 43201
+  // i:4 === 1 ri:0 === 4
+  // 13204
+
+  for (var i = 0; i < str.length; i++) {
+    var randIndex = getRandIndex(str)
+
+    var temp1 = str[i]
+    var temp2 = str[randIndex]
+
+    str[randIndex] = temp2
+    str[i] = temp1
+  }
+
+  return str.join("")
+}
